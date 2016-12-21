740575617900bbea37d672da2f117555ad6cf29a
paul <paul_lin@foxmail.com>
2016-12-21 10:42:31 +0800

    REF implement of spell checker

--- scala/BK-tree/src/main/scala-2.11/com/moon/Node.scala
+++ scala/BK-tree/src/main/scala-2.11/com/moon/Node.scala
@@ -1,52 +1,52 @@
 package com.moon
+
 /**
   * Created by Paul on 2016/12/20.
   */
-class Node[T](term:T) {
-  val value:T=term
+class Node[T](term: T) {
+  val value: T = term
   // 用一个map存储子节点
-  var children:Map[Double,Node[T]]=Map()
+  var children: Map[Double, Node[T]] = Map()
 
-  def add(metricSpace: MetricSpace[T],value:T): Unit ={
+  def add(metricSpace: MetricSpace[T], value: T): Unit = {
     // value与父节点的距离
-    val distance:Double=metricSpace.distance(this.value,value)
+    val distance: Double = metricSpace.distance(this.value, value)
 
     // 距离为0，表示元素相同，返回
-    if(distance == 0)
+    if (distance == 0)
       return
 
     // 从父节点的子节点查找child，满足距离为distance
-    val child:Node[T]=children.get(distance).get
-
+    val child: Option[Node[T]] = children.get(distance)
     child match{
-      case null =>{
+      case None =>{
         // 若距离父节点为distance的子节点不存在，则直接添加一个新的子节点
-        children+=(distance->new Node[T](value))
+        children += (distance -> new Node[T](value))
       }
       case _ =>{
         // 若距离父节点为distance子节点存在，则递归的将value添加到该子节点下
-        child.add(metricSpace,value)
+        child.get.add(metricSpace, value)
       }
     }
   }
 
-  def query(ms:MetricSpace[T],term:T,radius:Double,results:scala.collection.mutable.Set[T] ):Unit={
-    val distance:Double=ms.distance(this.value,term)
+  def query(ms: MetricSpace[T], term: T, radius: Double, results: scala.collection.mutable.Set[T]): Unit = {
+    val distance: Double = ms.distance(this.value, term)
 
     // 与父节点的距离小于阀值，则添加到结果集中，并继续向下寻找
-    if(distance <= radius)
-      results+(this.value)
+    if (distance <= radius)
+      results += (this.value)
 
     // 子节点的距离在最小距离和最大距离之间的
     // 由度量空间的d(x,y)+d(y,z) >= d(x,z)这一定理，有查找的value与子节点的距离范围如下:
     // min={1,distance-radius}, max=distance+radius
-    var max:Double=Math.max(distance-radius,1)
-    while(max < (distance+radius)){
-      val child:Option[Node[T]]=children.get(max)
+    var max: Double = Math.max(distance - radius, 1)
+    while (max < (distance + radius)) {
+      val child: Option[Node[T]] = children.get(max)
       // 递归调用
-      if(child.nonEmpty)
-        child.get.query(ms,term,radius,results)
-      max=max+1
+      if (child.nonEmpty)
+        child.get.query(ms, term, radius, results)
+      max = max + 1
     }
   }
 }
--- scala/BK-tree/src/main/scala-2.11/com/moon/impl/LevelsteinDistance.scala
+++ scala/BK-tree/src/main/scala-2.11/com/moon/impl/LevelsteinDistance.scala
@@ -3,6 +3,10 @@ package com.moon.impl
 import com.moon.MetricSpace
 
 /**
+  * 编辑距离， 又称Levenshtein距离，是指两个字串之间，由一个转成另一个所需的最少编辑操作次数。
+  * 该类中许可的编辑操作包括将一个字符替换成另一个字符，插入一个字符，删除一个字符。
+  *
+  * 使用动态规划算法。算法复杂度：m*n。
   * Created by Paul on 2016/12/20.
   */
 class LevelsteinDistance extends MetricSpace[String] {
@@ -10,40 +14,42 @@ class LevelsteinDistance extends MetricSpace[String] {
   var deleteCost: Double = 1
   var substituCost: Double = 1.5
 
-  def computeDistance(target:String ,source:String):Double={
-    val n:Int=target.trim.length
-    val m:Int=source.trim.length
+  def computeDistance(target: String, source: String): Double = {
+    val n: Int = target.trim.length
+    val m: Int = source.trim.length
 
-    var distance:Array[Array[Double]]=Array.ofDim[Double](n+1,m+1)
-    distance(0)(0)=0
+    val distance: Array[Array[Double]] = Array.ofDim[Double](n + 1, m + 1)
+    distance(0)(0) = 0
 
     for (i <- 1 to m)
-      distance(0)(i)=i
+      distance(0)(i) = i
 
-    for(j <- 1 to n)
-      distance(j)(0)=j
+    for (j <- 1 to n)
+      distance(j)(0) = j
 
-    for(i<- 1 to n){
-      for (j <- 1 to m){
-        var min:Double=distance(i-1)(j-1)
-        if(target.charAt(i-1)==source.charAt(j-1)){
-          if(min > distance(i-1)(j-1))
-            min=distance(i-1)(j-1)
-        }else{
-          if(min>distance(i-1)(j-1)+substituCost)
-            min=distance(i-1)(j-1)+substituCost
+    for (i <- 1 to n) {
+      for (j <- 1 to m) {
+        var min: Double = distance(i - 1)(j - 1) + insertCost
+        if (target.charAt(i - 1) == source.charAt(j - 1)) {
+          if (min > distance(i - 1)(j - 1))
+            min = distance(i - 1)(j - 1)
+        } else {
+          if (min > distance(i - 1)(j - 1) + substituCost)
+            min = distance(i - 1)(j - 1) + substituCost
         }
-        distance(i)(j)=min
+        if (min > distance(i)(j - 1) + deleteCost)
+          min = distance(i)(j - 1) + deleteCost
+        distance(i)(j) = min
       }
     }
     distance(n)(m)
   }
 
-  override def distance(a: String, b: String): Double = computeDistance(a,b)
+  override def distance(a: String, b: String): Double = computeDistance(a, b)
 
 
 }
 
-object LevelsteinDistance{
+object LevelsteinDistance {
 
 }
