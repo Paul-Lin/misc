3245eb81751a7dbd57788970eb0de749ecddb18d
paul <paul_lin@foxmail.com>
2017-02-06 17:20:25 +0800

    增加java web基本框架

--- java/framework/src/main/java/com/moon/DispatcherServlet.java
+++ java/framework/src/main/java/com/moon/DispatcherServlet.java
@@ -1,10 +1,19 @@
 package com.moon;
 
 import com.moon.annotation.Controller;
+import com.moon.bean.Data;
 import com.moon.bean.Handler;
+import com.moon.bean.Param;
+import com.moon.bean.View;
 import com.moon.helper.BeanHelper;
 import com.moon.helper.ControllerHelper;
 import com.moon.helper.HelperLoader;
+import com.moon.util.CodecUtil;
+import com.moon.util.JsonUtil;
+import com.moon.util.ReflectionUtil;
+import com.moon.util.StreamUtil;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -12,10 +21,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 
 /**
  * Created by Paul on 2017/2/3.
@@ -57,7 +69,53 @@ public class DispatcherServlet extends HttpServlet{
                 String paramValue=req.getParameter(paramName);
                 paramMap.put(paramName,paramValue);
             }
-//            String body=CodecUtil
+            String body= CodecUtil.decodeURL(StreamUtil.getString(req.getInputStream()));
+            if(StringUtils.isNotBlank(body)){
+                String[] params=body.split("&");
+                Stream.of(params).forEach(x->{
+                    String[] array=x.split("=");
+                    if(ArrayUtils.isNotEmpty(array)&&array.length==2){
+                        String paramName=array[0];
+                        String paramValue=array[1];
+                        paramMap.put(paramName,paramValue);
+                    }
+                });
+            }
+            Param param=new Param(paramMap);
+            // 调用Action方法
+            Method actionMethod=handler.getActionMethod();
+            Object result= ReflectionUtil.invokeMethod(controllerBean,actionMethod,param);
+            // 处理Action方法返回值
+            if(result instanceof View){
+                // 返回JSP页面
+                View view= (View) result;
+                String path=view.getPath();
+                if(StringUtils.isNotBlank(path)){
+                    if(path.startsWith("/")){
+                        resp.sendRedirect(req.getContextPath()+path);
+                    }else{
+                        Map<String,Object> model=view.getModel();
+                        model.entrySet().forEach(x->{
+                            req.setAttribute(x.getKey(),x.getValue());
+                        });
+                        String appJspPath="";
+                        req.getRequestDispatcher(appJspPath+path).forward(req,resp);
+                    }
+                }
+            }else if(result instanceof Data){
+                // 返回JSON数据
+                Data data=(Data) result;
+                Object model=data.getModel();
+                if(Objects.nonNull(model)){
+                    resp.setContentType("application/json");
+                    resp.setCharacterEncoding("UTF-8");
+                    PrintWriter writer=resp.getWriter();
+                    String json= JsonUtil.toJson(model);
+                    writer.write(json);
+                    writer.flush();
+                    writer.close();
+                }
+            }
         }
     }
 }
--- java/framework/src/main/java/com/moon/util/CodecUtil.java
+++ java/framework/src/main/java/com/moon/util/CodecUtil.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 /**
@@ -12,6 +13,12 @@ import java.net.URLEncoder;
  */
 public final class CodecUtil {
     private static final Logger LOGGER= LoggerFactory.getLogger(CodecUtil.class);
+
+    /**
+     * 将URL编码
+     * @param source
+     * @return
+     */
     public static String encodeURL(String source){
         String target;
         try {
@@ -21,5 +28,22 @@ public final class CodecUtil {
             throw new RuntimeException();
         }
         return target;
+    }
+
+    /**
+     * 将URL解码
+     * @param source
+     * @return
+     */
+    public static String decodeURL(String source){
+        String target;
+        try {
+            target= URLDecoder.decode(source,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error("decode url failure",e);
+            throw new RuntimeException(e);
+        }
+        return target;
+
     }
 }
